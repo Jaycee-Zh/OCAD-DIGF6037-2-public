7e5a5a9b0d4388af980a8fa3e87392aa14a45856
J.C. Zhang <jaycee.zh@yahoo.com>
2024-12-09 10:20:47 -0500

    36 pets, photo page and some fix
    - generated more pets
    - added the photo page
    - changed git setting to ignore line end issue

---
+++ photoSketch.js
@@ -0,0 +1,50 @@
+const videoSize = [1920,1080];
+const resolution = 40;
+
+function setup() {
+  createCanvas(videoSize[0], videoSize[1]);
+  video= createCapture(VIDEO, { flipped: true });
+  video.size(videoSize[0], videoSize[1]);
+  video.hide();
+
+  function mousePressed(){ //!debug
+    if(mouseX>0&& mouseX<videoSize[0] && mouseY>0 && mouseY<videoSize[1]){
+      let fs=fullscreen();
+      fullscreen(!fs);
+    }
+
+  }
+  
+  // random number
+  humanId = random(0,16581375).toString(16).toUpperCase();  
+  console.log(humanId);
+}
+
+function draw() {
+  background(220);
+  colorMode(RGB,255);
+  noStroke();
+
+    // Draw the webcam video
+    image(video, 0, 0, videoSize[0], videoSize[1]);
+
+    for (i=0;i*resolution < videoSize[1]; i++){ // y
+      for (j=0; j*resolution< videoSize[0]; j++){ //x
+        let colour = get(j*resolution, i*resolution);
+        // console.log(colour);
+        fill(colour);
+        square(j*resolution,i*resolution,resolution);
+      }
+    }
+
+    push();
+    rotate(HALF_PI);
+    textFont('Courier New', resolution);
+    textAlign(CENTER);
+    fill(0);
+    stroke(255);
+    strokeWeight(4);
+    // text("Human "+humanId, videoSize[0]/2-resolution*10, videoSize[1] - resolution*4 )
+    text("Human "+humanId, videoSize[1]/2, -resolution*2 )
+    pop();
+}
